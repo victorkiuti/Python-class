393b179709a7094810fe68b78c2b98fb036aedfc
victorkiuti <victorkiuti@gmail.com>
2025-01-13 20:14:32 -0800

    Add files via upload

---
+++ Quiz 1 - victor.py
@@ -0,0 +1,11 @@
+print ("What is your name?")
+x = input () 
+print ("where is your country?")
+country = input ()
+print ("Where do you study?")
+school = input ()
+print ("What is the location fo your school?")
+location = input ()
+
+print ("My name is " + x, "from " + country , "studying at " + school, "in " + location)
+
