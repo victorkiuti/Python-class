ce9a5b468762c4e65083d43c859cf4d3f7d9d706
victorkiuti <victorkiuti@gmail.com>
2025-01-18 14:39:20 -0800

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,20 @@
+#Problem 1
+A = 1996
+J = 1999
+
+if (A/4 == 499):
+    print ("Annie you were born in a leap year")
+elif (J/4 == 499):
+    print ("Jane you were born in a leap year")
+else:
+    print ("None of you were not born in a leap year")
+
+#Problem 2
+Sam = 10
+
+if Sam<9:
+    print ("your breakfeast is a milk porridge")
+elif(Sam>9) and (Sam<14):
+    print ("your breakfeast is a sandwich")
+else:
+    print ("your breakfeast is a burguer")
